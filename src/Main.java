92917c1c573c988f86252ff6af4742cefaf6ad49
RST90 <rafa.st_@hotmail.com>
2025-10-21 18:01:59 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 package PACKAGE_NAME;
 
 public class Main {
-    public class Main {
+    public static void Main(String[] args){
+        final String nombreAplicacion = "App 2.0";
+        int version =1.0.0;
+
+    }
     }
